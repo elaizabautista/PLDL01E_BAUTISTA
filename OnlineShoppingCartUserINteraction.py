eb09d264e1b82a707ecd14754c68d179ca171fcf
elaizabautista <2023-2-02500@lpunetwork.edu.ph>
2025-03-03 10:13:42 -0800

    Create OnlineShoppingCartUserINteraction.py

---
+++ OnlineShoppingCartUserINteraction.py
@@ -0,0 +1,42 @@
+C:\Users\acer\PycharmProjects\pythonProject\.venv\Scripts\python.exe C:\Users\acer\PycharmProjects\pythonProject\OnlineShoppingCartSystemUserInteraction.py 
+
+Available Products:
+1. Laptop (₱50000) - Warranty: 2 year(s)
+2. Smartphone (₱30000) - Warranty: 1 year(s)
+3. T-Shirt (₱500) - Size: M
+4. Jeans (₱1500) - Size: L
+5. Tablet (₱20000) - Warranty: 1 year(s)
+6. Headphones (₱3000) - Warranty: 1 year(s)
+7. Jacket (₱2500) - Size: XL
+8. Shoes (₱4000) - Size: 42
+
+1. Add Item to Cart
+2. View Cart
+3. Checkout
+4. Exit
+Enter choice: 1
+Enter item number to add to cart: 2
+Added: Smartphone (₱30000) - Warranty: 1 year(s)
+
+1. Add Item to Cart
+2. View Cart
+3. Checkout
+4. Exit
+Enter choice: 2
+
+Shopping Cart:
+1. Smartphone (₱30000) - Warranty: 1 year(s)
+
+1. Add Item to Cart
+2. View Cart
+3. Checkout
+4. Exit
+Enter choice: 3
+
+Total: ₱30000.00
+
+1. Add Item to Cart
+2. View Cart
+3. Checkout
+4. Exit
+Enter choice: 
