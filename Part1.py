f607ed38260175d5a9b078edaf09279bbb4c7959
elaizabautista <2023-2-02500@lpunetwork.edu.ph>
2025-03-31 08:41:54 -0700

    Create Part1.py

---
+++ Part1.py
@@ -0,0 +1,7 @@
+name = "Elaiza"
+new_name = name[:2] + "y" + name[3:]  # Create a modified string
+print(new_name)  # Outputs: Elyiza
+
+many_numbers = [4, 9, 12, 15, 5, 3, 110]
+many_numbers[2] = 5000  # Modify the list at index 2
+print(many_numbers)  # Outputs: [4, 9, 5000, 15, 5, 3, 110]
